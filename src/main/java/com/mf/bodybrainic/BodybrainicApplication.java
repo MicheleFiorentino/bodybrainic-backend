55e8248f0f8cb4cc6a5c01afa486e29cfcf4f792
MicheleFiorentino <fiorentino.mic@outlook.it>
2023-07-22 17:01:51 +0200

    First Update

--- src/main/java/com/mf/bodybrainic/BodybrainicApplication.java
+++ src/main/java/com/mf/bodybrainic/BodybrainicApplication.java
@@ -2,10 +2,10 @@ package com.mf.bodybrainic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BodybrainicApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BodybrainicApplication.class, args);
 	}
