d810b378d27769e0d48b3506230364a6b6bb0ff8
DevXino <itdevxino@gmail.com>
2023-07-22 11:40:42 +0200

    Initial commit

---
+++ src/main/java/com/mf/bodybrainic/BodybrainicApplication.java
@@ -0,0 +1,13 @@
+package com.mf.bodybrainic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodybrainicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodybrainicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mf/bodybrainic/BodybrainicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mf.bodybrainic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodybrainicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
